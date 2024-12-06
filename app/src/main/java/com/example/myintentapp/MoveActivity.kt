5fa915088a127fb11e0bd45027828ead4ebd9dbc
aflrizky <alfrizky625@gmail.com>
2024-12-06 20:30:53 +0700

    commit

---
+++ app/src/main/java/com/example/myintentapp/MoveActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMainActivity = findViewById<Button>(R.id.btn_main_activity)
+        btnMainActivity.setOnClickListener {
+            onClick()
+        }
+    }
+        private fun onClick() {
+            val back = Intent(applicationContext, MainActivity::class.java)
+            startActivity(back)
+            }
+}
